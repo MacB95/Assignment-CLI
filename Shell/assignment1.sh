b04641588f963d42a2037d7eb119bd2e0f6673d4
MacB95 <mackinleyblanchard@gmail.com>
2019-11-06 15:01:20 -0600

    Assignment

---
+++ Shell/assignment1.sh
@@ -0,0 +1,12 @@
+mkdir ~/Desktop/Projects/
+mkdir ~/Desktop/Projects/Assignment-CLI
+mkdir ~/Desktop/Projects/Assignment-CLI/Version-1
+mkdir ~/Desktop/Projects/Assignment-CLI/Version-2
+touch ~/Desktop/Projects/Assignment-CLI/Version-1/index.html
+touch ~/Desktop/Projects/Assignment-CLI/Version-1/contact-us.html
+touch ~/Desktop/Projects/Assignment-CLI/Version-1/services.html
+touch ~/Desktop/Projects/Assignment-CLI/Version-1/about-us.html
+touch ~/Desktop/Projects/Assignment-CLI/Version-1/index-2.html
+touch ~/Desktop/Projects/Assignment-CLI/Version-1/services-2.html
+mv ~/Desktop/Projects/Assignment-CLI/Version-1/index-2.html ~/Desktop/Projects/Assignment-CLI/Version-2
+mv ~/Desktop/Projects/Assignment-CLI/Version-1/services-2.html ~/Desktop/Projects/Assignment-CLI/Version-2
